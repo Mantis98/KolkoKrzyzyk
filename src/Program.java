5ae15ab27b21fdee01e2c1baf049a2e97bd372b8
Mateusz Kowalski <MateuszKowaalski@gmail.com>
2023-04-14 17:31:37 +0200

    implementing gameplay

--- src/Program.java
+++ src/Program.java
@@ -9,18 +9,28 @@ public class Program implements ActionListener {
     JButton[] buttons = new JButton[9];
     JButton one, two, three, four, five, six, seven, eight, nine;
     JPanel panel;
+    JTextField textField;
 
-    Font czcionka = new Font("Arial", Font.BOLD, 50);
+    Font font = new Font("Serif", Font.BOLD, 50);
     char kolko = 'O';
     char krzyzyk = 'X';
     boolean playerX_Turn = true;
 
     public Program() {
 
-        frame = new JFrame("Gra");
+        frame = new JFrame("Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
         frame.setLayout(null);
+
+
+        textField = new JTextField();
+        textField.setBounds(100, 400, 400, 80);
+        textField.setFont(font);
+        textField.setEditable(false);
+        textField.setHorizontalAlignment(SwingConstants.CENTER);
+        textField.setText("So it begins");
+
 
         one = new JButton("");
         two = new JButton("");
@@ -44,13 +54,14 @@ public class Program implements ActionListener {
 
         for (int i = 0; i < 9; i++) {
             buttons[i].addActionListener(this);
-            buttons[i].setFont(czcionka);
+            buttons[i].setFont(font);
             buttons[i].setFocusable(false);
         }
 
         panel = new JPanel();
-        panel.setBounds(130, 50, 300, 300);
+        panel.setBounds(150, 50, 300, 300);
         panel.setLayout(new GridLayout(3, 3, 15, 15));
+        panel.setBackground(Color.GRAY);
 
         panel.add(one);
         panel.add(two);
@@ -65,6 +76,7 @@ public class Program implements ActionListener {
 
         frame.add(panel);
         frame.setVisible(true);
+        frame.add(textField);
 
 
     }
@@ -79,11 +91,15 @@ public class Program implements ActionListener {
                     if (buttons[i].getText() == "") {
                         buttons[i].setText(String.valueOf(krzyzyk));
                         playerX_Turn = false;
+                        textField.setText("Player O turn");
+                        checkWinner();
                     }
                 } else {
                     if (buttons[i].getText() == "") {
                         buttons[i].setText(String.valueOf(kolko));
                         playerX_Turn = true;
+                        textField.setText("Player X turn");
+                        checkWinner();
                     }
                 }
 
@@ -91,4 +107,139 @@ public class Program implements ActionListener {
 
         }
     }
+
+    public void checkWinner() {
+        if (
+                (one.getText().equals("X")) &&
+                        (two.getText().equals("X")) &&
+                        (three.getText().equals("X"))
+        ) {
+            xWins();
+        }
+        if (
+                (four.getText().equals("X")) &&
+                        (five.getText().equals("X")) &&
+                        (six.getText().equals("X"))
+        ) {
+            xWins();
+        }
+        if (
+                (seven.getText().equals("X")) &&
+                        (eight.getText().equals("X")) &&
+                        (nine.getText().equals("X"))
+        ) {
+            xWins();
+        }
+        if (
+                (one.getText().equals("X")) &&
+                        (four.getText().equals("X")) &&
+                        (seven.getText().equals("X"))
+        ) {
+            xWins();
+        }
+        if (
+                (two.getText().equals("X")) &&
+                        (five.getText().equals("X")) &&
+                        (eight.getText().equals("X"))
+        ) {
+            xWins();
+        }
+        if (
+                (three.getText().equals("X")) &&
+                        (six.getText().equals("X")) &&
+                        (nine.getText().equals("X"))
+        ) {
+            xWins();
+        }
+        if (
+                (one.getText().equals("X")) &&
+                        (five.getText().equals("X")) &&
+                        (nine.getText().equals("X"))
+        ) {
+            xWins();
+        }
+        if (
+                (three.getText().equals("X")) &&
+                        (five.getText().equals("X")) &&
+                        (seven.getText().equals("X"))
+        ) {
+            xWins();
+        }
+        if (
+                (one.getText().equals("O")) &&
+                        (two.getText().equals("O")) &&
+                        (three.getText().equals("O"))
+        ) {
+            oWins();
+        }
+        if (
+                (four.getText().equals("O")) &&
+                        (five.getText().equals("O")) &&
+                        (six.getText().equals("O"))
+        ) {
+            oWins();
+        }
+        if (
+                (seven.getText().equals("O")) &&
+                        (eight.getText().equals("O")) &&
+                        (nine.getText().equals("O"))
+        ) {
+            oWins();
+        }
+        if (
+                (one.getText().equals("O")) &&
+                        (four.getText().equals("O")) &&
+                        (eight.getText().equals("O"))
+        ) {
+            oWins();
+        }
+        if (
+                (two.getText().equals("O")) &&
+                        (five.getText().equals("O")) &&
+                        (eight.getText().equals("O"))
+        ) {
+            oWins();
+        }
+        if (
+                (three.getText().equals("O")) &&
+                        (six.getText().equals("O")) &&
+                        (nine.getText().equals("O"))
+        ) {
+            oWins();
+        }
+        if (
+                (one.getText().equals("O")) &&
+                        (five.getText().equals("O")) &&
+                        (nine.getText().equals("O"))
+        ) {
+            oWins();
+        }
+        if (
+                (three.getText().equals("O")) &&
+                        (five.getText().equals("O")) &&
+                        (seven.getText().equals("O"))
+        ) {
+            oWins();
+
+        }
+
+
+    }
+
+    public void xWins() {
+        for (int i = 0; i < 9; i++) {
+            buttons[i].setEnabled(false);
+        }
+        textField.setText("Player X wins!");
+    }
+
+    public void oWins() {
+        for (int i = 0; i < 9; i++) {
+            buttons[i].setEnabled(false);
+        }
+        textField.setText("Player O wins!");
+    }
+
+
+
 }
